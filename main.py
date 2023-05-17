59eaadfa299b747cde7f4fcb696c8b7db3fd5a19
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-17 20:10:17 -0300

    Implementando cnpj a classe

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from cpf import Cpf
+from cpf_cnpj import CpfCnpj
 
-cpf_um = Cpf("15316264754")
-print(cpf_um)
+#cpf_um = Cpf("15316264754")
+#print(cpf_um)
+exemplo_cnpj = "35379838000112"
+#cnpj = CNPJ()
+#print(cnpj.validate(exemplo_cnpj))
+
+documento = CpfCnpj(exemplo_cnpj, 'cnpj')

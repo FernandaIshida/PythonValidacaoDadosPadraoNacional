ddbba09c71d96bc11b1bedd970000eb6e8782e89
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-17 23:32:54 -0300

    Adicionando mascara telefone

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from telefones_br import TelefonesBr
+import re
 
 telefone = "552126481234"
+
 telefone_objeto = TelefonesBr(telefone)
+#padrao = "([0-9]{2,3})?([0-9]{2})?([0-9]{4,5})([0-9]{4})"
+#resposta = re.search(padrao, telefone)
+#print(resposta.group(1))
+print(telefone_objeto)

6c10b051c346e4ec135c49e57334ef3f044f8a2d
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-18 00:07:32 -0300

    Classe Datasbr

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from telefones_br import TelefonesBr
-import re
+from datetime import datetime, timedelta
+from datas_br import DatasBr
 
-telefone = "552126481234"
+cadastro = DatasBr()
+print(cadastro.momento_cadastro)
 
-telefone_objeto = TelefonesBr(telefone)
-#padrao = "([0-9]{2,3})?([0-9]{2})?([0-9]{4,5})([0-9]{4})"
-#resposta = re.search(padrao, telefone)
-#print(resposta.group(1))
-print(telefone_objeto)

2ca47027cd9099af912659b6d1f5dbd785179db0
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-18 00:20:16 -0300

    Mascara data

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from datetime import datetime, timedelta
 from datas_br import DatasBr
 
 cadastro = DatasBr()
-print(cadastro.momento_cadastro)
+print(cadastro)
 
+'''
+hoje = datetime.today()
+hoje_formatado = hoje.strftime("%d/%m/%Y %H:%M")
+print(hoje)
+print(hoje_formatado)
+print(type(hoje_formatado))
+'''

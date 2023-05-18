5abb0b5ab170debff27a7a47284191cd7bea8c96
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-17 23:11:03 -0300

    Classe TelefonesBr

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from cpf_cnpj import Documento
+from telefones_br import TelefonesBr
 
-#cpf_um = Cpf("15316264754")
-#print(cpf_um)
-#exemplo_cnpj = "35379838000112"
-exemplo_cpf = "15316264754"
-#cnpj = CNPJ()
-#print(cnpj.validate(exemplo_cnpj))
-
-documento = Documento.cria_documento(exemplo_cpf)
-print(documento)
+telefone = "552126481234"
+telefone_objeto = TelefonesBr(telefone)

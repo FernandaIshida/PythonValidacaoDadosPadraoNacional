396e8e6eb8a2e93e9b98a63bbd9d6a8053b0257b
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-22 15:28:30 -0300

    Acessando IPA com Python

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import requests
 from acesso_cep import BuscaEndereco
-
-cep = 25870145
+cep = "01001000"
 objeto_cep = BuscaEndereco(cep)
-print(objeto_cep)
+
+
+#r = requests.get("https://viacep.com.br/ws/01001000/json/")
+#print(type(r.text))
+
+a = objeto_cep.acessa_via_cep()
+print(a)
